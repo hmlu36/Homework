e583f344524f71c38b8e19dd89f5b09f72206199
Joshua <joshua.lu@smms.com.tw>
2024-01-22 17:02:24 +0800

    作業1更新

---
+++ Controllers/Homework1Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace homework1.Controllers
+{
+    /// <summary>
+    /// Homework 1
+    /// </summary>
+    [ApiController]
+    [Route("api/HelloWorld")]
+    public class Homework1Controller : ControllerBase
+    {
+
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World!";
+        }
+    }
+}

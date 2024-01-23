28407aa2d484470973032967073510d4d7dc97d9
Joshua <joshua.lu@smms.com.tw>
2024-01-23 11:43:07 +0800

    作業2

---
+++ Enums/Sex.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework.Enums
+{
+    public enum Sex
+    {
+        /// <summary>
+        /// 男性
+        /// </summary>
+        Male,
+
+        /// <summary>
+        /// 女性
+        /// </summary>
+        Female
+    }
+}
